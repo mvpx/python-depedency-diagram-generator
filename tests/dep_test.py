673a72426d18abda95426f30536f6ca3c3a381cf
Luis Yoshida <luis.yoshida@gmail.com>
2025-05-03 15:23:57 -0300

    update test file

--- tests/dep_test.py
+++ tests/dep_test.py
@@ -9,10 +9,20 @@ class C:
     def __init__(self, d: D):
         self.d = d
 
+class CA:
+    def __init__(self, d: D):
+        self.d = D
+
 class B:
-    def __init__(self, c: C):
+    def __init__(self, c: C, ca: CA):
         self.c = c
+        self.ca = ca
+        test_func()
 
 class A:
     def __init__(self, b: B):
         self.b = b
+
+
+def test_func():
+    pass
